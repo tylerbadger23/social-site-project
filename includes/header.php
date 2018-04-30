2f8d24b453dc266fb0b4c484908aab9ea036dd9d
tyler laceby <tylerbadger23@gmail.com>
2018-04-30 13:15:08 -0600

    styled top bar
    :)

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<?php
+require 'config/config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Social Site</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+<body>
+
+<div class="top-bar">
+    <div class="logo"> <a href="index.php">Social Site</a></div>    
+
+    <nav>
+        <a href="#"><i class="fa fa-home "></i></a>
+        <a href="#"><i class="fa fa-envelope"></i></a>
+        <a href="#"><i class="fa fa-bell "></i></a>
+        <a href="#"><i class="fa fa-users "></i></a>
+        <a href="#"><i class="fa fa-cog "></i></a>
+    </nav>
+</div>
--- index.php
+++ index.php
@@ -1,17 +1,19 @@
-<?php
-require 'config/config.php';
+<?php 
+
+include "includes/header.php";
+
+if(isset($_SESSION['username'])) {
+    $user_logged_in = $_SESSION['username'];
+} else {
+    header("Location: login.php");
+}
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
-</head>
-<body>
-    
+
+
+
+
+
 </body>
 </html>
